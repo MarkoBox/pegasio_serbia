8b41ff881e0f1383d64fb37fbb840da597d485fd
Marko Bokic <markoboki@hotmail.com>
2019-01-26 18:44:18 +0100

    Dodat model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+from pegasio_serbia.users.models import User
 
-# Create your models here.
+
+class Clients(models.Model):
+    name = models.CharField(max_length=255)
+    alternative_name = models.CharField(max_length=255, null=True, blank=True)
+    period_from = models.DateField()
+    period_to = models.DateField()
+    path_grps = models.CharField(max_length=255, null=True, blank=True)
+    path_alfresco = models.CharField(max_length=255, null=True, blank=True)
+    chr_flag = models.BooleanField()
+    e_comerce_flag = models.BooleanField()
+    vat = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'client'
+        verbose_name_plural = 'clients'
+
+
+class Batches(models.Model):
+    name = models.CharField(max_length=255)
+    period = models.DateField()
+    date_time_uploaded = models.DateTimeField(auto_now_add=True)
+    file_sent_to_accountant = models.FileField(null=True, blank=True, upload_to='files_sent/')
+    file_codified = models.FileField(null=True, blank=True, upload_to='files_codified/')
+    gl_export = models.FileField(null=True, blank=True)
+    pieces_generated = models.BooleanField(default=False)
+    accountant_name = models.ForeignKey(User, on_delete=models.PROTECT)
+    client_name = models.ForeignKey(Clients, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.client_name}'
+
+    class Meta:
+        verbose_name = 'batch'
+        verbose_name_plural = 'batches'
+
+    def generate_pieces(self):
+        # funkciju sa admina prebaci ovde, napravi check da ako je gore polje checkirano da ne pravi novi batch.
+        pass
+
+
+def piece_upload_path(instance, filename):
+    return f'pieces/{instance.batch.name}/{filename}'
+
+
+class Pieces(models.Model):
+    ACHATS = 'Achats'
+    BANQUE = 'Banque'
+    VENTES = 'Ventes'
+    OD = 'OD'
+    REJECTS = 'Rejects'
+    INCOMPLETE = 'Incomplete'
+    # dodaj ostale
+    FOLDER_CHOISES = (
+        (ACHATS, 'Achats'),
+        (BANQUE, 'Banque'),
+        (VENTES, 'Ventes'),
+        (REJECTS, 'Rejects'),
+        (INCOMPLETE, 'Incomplete'),
+        (OD, 'OD')
+    )
+    # dodaj regex validatore na codification polje i period polje
+    file = models.FileField(null=True, blank=True, upload_to=piece_upload_path)
+    file_name = models.CharField(max_length=255)
+    folder_original = models.CharField(max_length=255, null=True, blank=True)
+    codification = models.CharField(max_length=255, null=True, blank=True)
+    period = models.CharField(max_length=255, null=True, blank=True)
+    folder_assigned = models.CharField(max_length=255, choices=FOLDER_CHOISES, null=True,
+                                       blank=True)
+    booked = models.BooleanField(default=False)
+    batch = models.ForeignKey(Batches, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file_name
+
+    class Meta:
+        ordering = ['folder_original', 'file_name']
+        verbose_name = 'piece'
+        verbose_name_plural = 'pieces'
+
+
+class BatchTracking(models.Model):
+    reviewer = models.ForeignKey(User, on_delete=models.PROTECT)
+    preparer = models.ForeignKey(User, on_delete=models.PROTECT)
+    sent_to_accountant = models.BooleanField(default=False)
+    archived_sent_batch = models.BooleanField(default=False)
+    booked_and_codified = models.BooleanField(default=False)
+    controlled = models.BooleanField(default=False)
+    sent_back_to_accountant = models.BooleanField(default=False)
+    archived_to_alfresco = models.BooleanField(default=False)
+    archived_to_grps = models.BooleanField(default=False)
+    archived_of_gl_export = models.BooleanField(default=False)
+    batch = models.OneToOneField(Batches, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.batch.name
+
+    class Meta:
+        verbose_name = 'batch tracking'
+        verbose_name_plural = 'batch tracking'
