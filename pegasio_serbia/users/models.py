7d0f162eda4bb5480e2c3ca535ccb8baf21646db
Marko Bokic <markoboki@hotmail.com>
2019-01-26 20:23:23 +0100

    Removed user type contraint

--- pegasio_serbia/users/models.py
+++ pegasio_serbia/users/models.py
@@ -16,7 +16,7 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    user_type = IntegerField(choices=USER_TYPES)
+    user_type = IntegerField(choices=USER_TYPES, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
